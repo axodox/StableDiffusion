c4e91b60f4a666801b81e3224a63cb953d45b7ef
Peter Major <peter.major@cae.com>
2023-04-08 01:26:26 +0200

    Remove unnecessary files

--- NativeDiffusion/pch.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <Unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.AI.MachineLearning.h>
-
-#include <windows.ai.machinelearning.native.h>
-
-#include <MLOperatorAuthor.h>
